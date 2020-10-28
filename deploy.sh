534f39997076e992f372c740f8d8537d42454a52
flamendless <flamendless8@gmail.com>
2020-10-28 14:19:46 +0800

    Fixed deployment path

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,10 @@ yarn run build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'zion-english.com' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
 git push -f https://github.com/flamendless/zion-english-vue.git master:gh-pages
+# git push -f https://github.com/flamendless/zion-english.git master
 cd -
