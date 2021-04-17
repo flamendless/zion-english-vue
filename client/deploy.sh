2ec1e82b093764b74048a1bc6612ee48ab5e17f0
flamendless <flamendless8@gmail.com>
2021-04-17 11:21:21 +0800

    Updated build

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env sh
-# abort on errors
-set -e
-# build
-yarn run build
-# navigate into the build output directory
-cd dist
-# if you are deploying to a custom domain
-echo 'zion-english.com' > CNAME
-git init
-git add -A
-git commit -m 'deploy'
-git push -f https://github.com/flamendless/zion-english-vue.git master:gh-pages
-# git push -f https://github.com/flamendless/zion-english.git master
-cd -
+msg=
+
+function deploy() {
+	# abort on errors
+	set -e
+	# build
+	yarn run build
+	# navigate into the build output directory
+	cd dist
+	# if you are deploying to a custom domain
+	echo 'zion-english.com' > CNAME
+	git init
+	git add -A
+	git commit -m "${msg}"
+	git push -f https://github.com/flamendless/zion-english-vue.git master:gh-pages
+	# git push -f https://github.com/flamendless/zion-english.git master
+	cd -
+}
+
+if [ $# -eq 1 ]; then
+	msg=$1
+	deploy
+else
+	echo "must pass in a commit message"
+fi
